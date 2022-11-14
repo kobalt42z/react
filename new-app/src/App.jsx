b4e48c38fbd7a76180876d545c6e0ac332604f89
kobalt42z <chmouel2690@gmail.com>
2022-11-14 15:50:09 +0200

    new

---
+++ new-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { ReactComponent as Logo } from './img/logo-netflix.svg';
+import './App.css';
+import Header from "./components/Header";
+{/* <Logo className="NetflixLogo" /> */}
+function App() {
+  return (
+
+    <div className="App ">
+      <div className="App-header">
+        <Header/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ new-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react';
+const [state, setstate] = useState(initialState);
+
+function Header() {
+  return (
+    <div>Header : </div>
+  )
+}
+
+export default Header
