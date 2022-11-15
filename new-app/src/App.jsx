51ad11a3d67bdcde656e81fc0bffdcd68996f4e4
Konstantin <kostyaaa2468@gmail.com>
2022-11-15 17:24:38 +0200

    Block4

--- new-app/src/App.jsx
+++ new-app/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from "./components/header/Header";
 import Block2 from './components/block2/block2';
+import Block4 from './components/block4/block4';
 function App() {
   return (
 
@@ -9,6 +10,7 @@ function App() {
       <div className="App-header">
         <Header/>
         <Block2/>
+        <Block4/>
       </div>
     </div>
   );
---
+++ new-app/src/components/block4/block4.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './block4.css'
+import Leftside from './leftSide.png'
+function block4() {
+  return (
+    <div className="container-fluid d-flex justify-content-center">
+        <div className="leftside col-5">
+            <img className='ps-5 ms-5 col-10' src={Leftside} alt={"PhotoOfLeftSide"} />
+        </div>
+        <div className='rightside col-5 d-flex align-items-center'>
+        <div className=" col-10 d-flex flex-column justify-content-center text-start">
+           <h1>Create profiles for kids.</h1>
+           <h4>Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</h4>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default block4
